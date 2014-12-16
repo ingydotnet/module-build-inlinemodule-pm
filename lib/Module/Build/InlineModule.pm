cd5fce6441cf3bd1864bb5e05c9a5f4d20fd1e5d
Ingy döt Net <ingy@ingy.net>
2014-12-16 00:45:58 -0500

    First draft. Not Working yet.

--- lib/Module/Build/InlineModule.pm
+++ lib/Module/Build/InlineModule.pm
@@ -1,2 +1,21 @@
 package Module::Build::InlineModule;
 our $VERSION = '0.01';
+
+use base 'Module::Build';
+__PACKAGE__->add_property('inline');
+
+use Inline::Module();
+
+use XXX;
+
+sub ACTION_code {
+    my ($self) = @_;
+    my $module = $self->{properties}{inline}{module};
+    eval "require $module; 1" or die @_;
+}
+
+sub ACTION_distdir {
+    XXX "ACTION_distdir", @_;
+}
+
+1;
